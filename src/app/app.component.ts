f88f70d47bf22e52557310ca19c4abba97ed9081
rasikag <rasikagayan90@gmail.com>
2018-06-13 02:35:59 +0800

    Binding to Custom Properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  serverElements = [];
+  serverElements = [{
+    type: 'server',
+    name: 'TestServer',
+    content: 'This is my first test server'
+  }];
 
 }
